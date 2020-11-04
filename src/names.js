46fd44e9e46b256531c88227b2e0373be4980280
jared <jared.jacobson.2000@gmail.com>
2020-11-04 16:31:20 -0700

    fixed JSON fetching

--- src/names.js
+++ src/names.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import NameComponent from './name-component'
+import { localHost } from './Globals'
 
 class Names extends Component {
     constructor(props) {
         super(props);
         this.state = { names: []}
-        // this.getJSONData()
     }
     getJSONData() {
-        fetch('../../MOCK_DATA.json')
-            .then(response => response.json)
-            .then(names => this.setState({names: names}))
+        fetch(localHost + 'MOCK_DATA.json')
+            .then( response => response.json() )
+            .then(names => this.setState({ names: names }))
     }
     componentDidMount() {
         this.getJSONData()
