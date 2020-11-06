f04a94f3898a9985e4181801d86746758840ac8b
jared <jared.jacobson.2000@gmail.com>
2020-11-06 11:27:54 -0700

    added the searchDB method

--- application/src/search.js
+++ application/src/search.js
@@ -1,15 +1,29 @@
-import { Component } from 'react'
+import { Component } from 'react';
+import { server } from './Globals';
 
 class SearchBar extends Component {
     constructor(props) {
         super(props);
         this.state = { value: '' }
     }
-    handleChange = (event) => {
+    handleChange = async (event) => {
         event.preventDefault();
         this.setState({
             value: event.target.value
         });
+        await this.searchDB(this.state.value);
+    }
+    async searchDB(search) {
+        const names = async (name) =>
+            await fetch(`${server}names?${name}Name_like=${search}`)
+                .then(response => response.json());
+        const firstNames = await names('first');
+        const lastNames = await names('last');
+        const allTheNames = firstNames
+            .map(name => name.id)
+            .concat(lastNames.map(name => name.id))
+            .sort()
+            .filter((id, index, names) => names.indexOf(id) === index);
     }
     render() { 
         return (<input
